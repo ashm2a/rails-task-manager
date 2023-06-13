fba999e28a3a328673f632de654808df801ad521
audreysantoni <santoni.audrey@gmail.com>
2023-06-13 16:44:28 +0200

    exo fini sans style

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get "tasks", to: "tasks#index"
+  # get "tasks/new", to: "tasks#new", as: :task_new
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  # patch "tasks/:id", to: "tasks#update"
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # delete "tasks/:id", to: "tasks#destroy"
+  Rails.application.routes.draw do
+    resources :tasks
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
